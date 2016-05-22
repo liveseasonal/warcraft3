5731c7ba5334f57bae4b8550b90dbf5f55d95d0e
liveseasonal <matt@liveseasonal.com>
2016-05-22 18:17:13 +0000

    Finished product

--- lib/footman.rb
+++ lib/footman.rb
@@ -1,10 +1,23 @@
 # http://classic.battle.net/war3/human/units/footman.shtml
 
-class Footman
+class Footman < Unit
+
+  attr_reader :health_points, :attack_power
 
   def initialize
     # Need to default the 2 instance variables here
     # Also also give code outside this class access to these variables (via attr_reader, attr_writer or attr_accessor)
+
+    @health_points = 60
+    @attack_power = 10
   end
+
+  def attack!(enemy)
+    enemy.damage(10)  
+  end  
+
+  def damage(attack_power)
+    @health_points -= attack_power
+  end   
 
 end
